8fb993e0f58fb4ca40c9059e090f3271437df223
Ulisses <iamurise@gmail.com>
2023-01-25 16:17:36 -0300

    add notes

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,3 +1,10 @@
-export async function load(){
-    return {a:10}
+import type { Note } from "../types/note";
+
+export async function load() {
+    const notes: Note[] = [
+        { title: 'hello world', description: "it's me, Ulisses" },
+        { title: 'the future of humanity', description: 'self replicate nano machines' }
+    ]
+
+    return { notes }
 }
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,4 +1,18 @@
-<div class="p-4">
-  <h1 class="text-lg">From You</h1>
-  <div class="" />
+<script lang="ts">
+  import type { PageData } from "./$types";
+
+  export let data: PageData;
+</script>
+
+<div class="p-4 bg-zinc-800 flex-1">
+  <h1 class="text-xl text-white">From You</h1>
+  <h2 class="text-white opacity-50 mb-2">Applicativo de notas</h2>
+  <ul class="flex flex-row gap-2 flex-wrap">
+    {#each data.notes as post}
+      <li class="bg-yellow-100 p-2 shadow-xl rounded">
+        <h3 class="font-semibold text-lg">{post.title}</h3>
+        <div>{post.description}</div>
+      </li>
+    {/each}
+  </ul>
 </div>
