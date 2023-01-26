c3951f3731dbafd906e8ba37b541750f272a90fe
Ulisses <iamurise@gmail.com>
2023-01-26 13:52:43 -0300

    replace flex by grid on page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -7,7 +7,9 @@
 </script>
 
 <div class="p-4 flex-1 gap-4">
-  <div class="flex-row flex-wrap gap-2 w-full justify-between items-start">
+  <div
+    class="grid grid-cols-1 md:grid-cols-3 grid-flow-row md:grid-flow-col gap-2 items-start"
+  >
     <div>
       <h1 class="text-xl" style="font-family: 'Carter One';">From You</h1>
       <h2 class="opacity-50 text-sm">Applicativo de notas</h2>
