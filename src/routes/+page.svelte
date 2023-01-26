7b9b897e000fd09807646bb7e81e3f524c43700d
Ulisses <iamurise@gmail.com>
2023-01-26 12:38:04 -0300

    move add note form to routes

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import type { PageData } from "./$types";
-  import AddNoteForm from "../components/addNoteForm.svelte";
+  import AddNoteForm from "./addNoteForm.svelte";
   import Masonry from "./noteList.svelte";
   import SearchInput from "./searchInput.svelte";
   export let data: PageData;
