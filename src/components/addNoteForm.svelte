14cc76e33d91903b22e695e17874a1fd73ffbd30
Ulisses <iamurise@gmail.com>
2023-01-26 08:59:24 -0300

    change add note form style

--- src/components/addNoteForm.svelte
+++ src/components/addNoteForm.svelte
@@ -1,6 +1,6 @@
 <script lang="ts">
   import { enhance } from "$app/forms";
-  let title = "";
+  let title = "quale";
   let description = "";
 </script>
 
@@ -8,33 +8,33 @@
   use:enhance
   method="POST"
   id="add-note-form"
-  class="border-2 border-gray/50 rounded p-2 w-full max-w-sm"
+  class="border-2 border-gray/50 rounded w-full max-w-sm"
 >
   <input
     type="text"
     name="title"
     bind:value={title}
     placeholder="Título"
-    class="focus-underline bg-transparent font-semibold placeholder:text-gray-600"
+    class="m-2 focus-underline bg-transparent font-semibold placeholder:text-gray-600"
   />
   <textarea
     rows="1"
     name="description"
     placeholder="descrição"
     bind:value={description}
-    class="focus-underline bg-transparent min-h-[1.5rem] placeholder:text-gray-600"
+    class="mx-2 focus-underline bg-transparent min-h-[1.5rem] placeholder:text-gray-600"
   />
   {#if title || description}
     <input
       type="submit"
       value="salvar"
-      class="p-1 px-2 bg-green-600 rounded-lg mt-2 cursor-pointer"
+      class="p-1 px-2 bg-green-600 rounded mt-2 cursor-pointer text-white"
     />
   {/if}
 </form>
 
 <style lang="postcss">
   .focus-underline {
-    @apply outline-none focus:border-b focus:mb-0 mb-[1px];
+    @apply outline-none focus:border-b-2 focus:mb-0 mb-[2px];
   }
 </style>
