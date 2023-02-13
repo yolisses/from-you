1c3ba42d1f462304a337b1cacabb78d6ba06dde9
Ulisses <iamurise@gmail.com>
2023-02-13 13:02:12 -0300

    fix type error

--- src/routes/user/userButton.svelte
+++ src/routes/user/userButton.svelte
@@ -2,7 +2,7 @@
   import LogoutButton from "./logoutButton.svelte";
   import SignInButton from "./signInButton.svelte";
 
-  export let logged: boolean;
+  export let logged: boolean | undefined;
 </script>
 
 {#if logged}
